3fbdc0117676e51e8f9c8280b333e37aabcd70b1
TutopediaEdu <109454215+TutopediaEdu@users.noreply.github.com>
2023-08-05 18:59:40 +0000

    const data types

---
+++ js_practice/const_var.js
@@ -0,0 +1,11 @@
+// Memory space to store the values
+const account_id=12345;
+let email_id="tutopediaedu@gmail.com";
+var password="11111";
+city="Pune";
+//  account_id = 2; // This is not allowed for constant
+email_id="tutopedia@gmail.com";
+password="22222";
+city="Delhi";
+ console.log(account_id);
+ console.table([account_id,email_id,password,city]);
---
+++ js_practice/data_types.js
@@ -0,0 +1,24 @@
+"use script"; // treat all JS code as newer version
+// alert("Hello"); //we are using nodejs, not browser
+console.log(2+3);
+// console.log("Hello") console.log("World"); //This is not allowed
+console.log("This is ")
+console.log("allowed")
+
+let name="Tutopedia"
+let age = 18
+let isInstitute = true
+//number => 2 to the power 52
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => variable declared but value not assigned
+//symbol => used for uniquqeness
+
+
+//object
+
+console.log(typeof "Tutopedia"); //This is string
+console.log(typeof null); //This is an object
+console.log(typeof undefined); //This is undefined
