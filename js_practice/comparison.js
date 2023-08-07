aae906eb1e7a9d4ffdb263cdf3dab6c5ea1fbd58
TutopediaEdu <109454215+TutopediaEdu@users.noreply.github.com>
2023-08-07 03:37:32 +0000

    comparison

---
+++ js_practice/comparison.js
@@ -0,0 +1,11 @@
+console.log(2>1)
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+//=== strictly check along with the comparison of data type as well
+console.log("2" == 2)
+console.log("2" === 2)
+console.log(2 === 2)
---
+++ js_practice/operations_conversion.js
@@ -0,0 +1,27 @@
+let value = 5
+let negValue = -value
+console.log(negValue)
+console.log(5+2)
+console.log(5-2)
+console.log(5*2)
+console.log(5/2)
+console.log(5**2)
+console.log(5%3)
+
+let s1="Tutopedia"
+let s2=" An Education & IT Lab"
+console.log(s1+s2)
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log("1"+(2+2))
+console.log(1+2+"2")
+console.log(true)
+console.log(+true)
+console.log(+"")
+let gameCounter =100
+console.log(gameCounter)
+gameCounter++
+console.log(gameCounter)
+gameCounter--
+console.log(gameCounter)
